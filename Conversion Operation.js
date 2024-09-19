40194c0dc5d83d0204266d76d022ab2d1369112b
Soham kundu <151546383+code-with-soham@users.noreply.github.com>
2024-09-19 21:32:28 +0530

    Add files via upload

---
+++ Conversion Operation.js
@@ -0,0 +1,52 @@
+//              ✅ Datatype Conversion ....
+
+//                🔱 Number
+
+let score = "007"                    // String value
+console.log(score)                  // Print String Value
+let ValueInNumber =Number(score)    // Convert String into Number
+console.log(typeof(ValueInNumber))  // print type of variable
+console.log(ValueInNumber)          // print value
+
+
+/*
+        "10"    ➡🔜 10 (output)
+        "10abc" ➡🔜  NaN (output)
+        "abc"   ➡🔜 NaN (output)
+        "10.23" ➡🔜 10.23 (output)
+        " "     ➡🔜 0 (output)
+        "007"   ➡🔜 7 (output)
+        "true"  ➡🔜 1 (output)
+        "false" ➡🔜 0 (output)
+        "null"  ➡🔜 0 (output)
+        "undefined" ➡🔜 NaN (output)
+        "NaN"   ➡🔜 NaN (output)       
+*/
+
+//                🔱 Boolean
+
+let boolean = true                   // String value
+console.log(boolean)                  // Print String Value
+let ValueInboolean =Boolean(score)    // Convert String into Number
+console.log(typeof(ValueInboolean))  // print type of variable
+console.log(ValueInboolean)          // print value
+
+/*
+        true   = 1
+        false  = 0
+        ""     = false
+        "soham"= true
+
+        
+*/
+
+
+//                🔱 String
+
+let String = true                     // String value
+console.log(String)                  // Print String Value
+let ValueInstring =String(score)     // Convert String into Number
+console.log(typeof(ValueInstring))    // print type of variable
+console.log(ValueInstring)            // print value
+
+
